69d7686ced0f71522d0402bdacdda91804327ac6
Kathy Cuevas <meetnerdykat@gmail.com>
2019-06-05 22:47:20 -0400

    Implemented React Router Dom and routes are working!

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
@@ -6,13 +7,24 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          {/* <a href="">Home</a> */}
+          <Link className="text-primary" to="/home">
+            Home
+          </Link>
         </div>
+
         <div>
-          <a href="">About</a>
+          {/* <a href="">About</a> */}
+          <Link className="text-primary" to="/about">
+            About
+          </Link>
         </div>
+
         <div>
-          <a href="">Contact</a>
+          {/* <a href="">Contact</a> */}
+          <Link className="text-primary" to="/contact">
+            Contact
+          </Link>
         </div>
       </div>
     </div>
